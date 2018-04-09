5f9759246a5bfa8711d152dc3185a17acef0f596
Viktharien <viktharien@zoho.com>
2018-04-09 21:33:43 +0000

    set up basic crud actions for messages controller

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
     after_save :broadcast
     
     def broadcast
-       ActionCable.server.broadcast ‘web_notifications_channel’,
+       ActionCable.server.broadcast 'web_notifications_channel',
 message: '<p>Hello World!</p>' 
     end
 end
