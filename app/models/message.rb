f2b3a875cef460935930ed6fca6d316157e79884
Viktharien <viktharien@zoho.com>
2018-04-09 21:30:27 +0000

    generate message model, migration, and controller

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,8 @@
 class Message < ApplicationRecord
+    after_save :broadcast
+    
+    def broadcast
+       ActionCable.server.broadcast ‘web_notifications_channel’,
+message: ‘<p>Hello World!</p>’ 
+    end
 end
