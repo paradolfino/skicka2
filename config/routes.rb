37800520f82d451924b1218154aa4c0eb385b269
Viktharien <viktharien@zoho.com>
2018-04-09 21:29:00 +0000

    generate message model, migration, and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  get 'messages/show'
+  get 'messages/new'
+  get 'messages/create'
+  get 'messages/edit'
+  get 'messages/update'
+  get 'messages/destroy'
   get 'page/index'
   root 'page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
