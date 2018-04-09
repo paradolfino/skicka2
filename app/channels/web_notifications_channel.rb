23a969ad324b030f2d737f780a77d62cdad9ed2f
Viktharien <viktharien@zoho.com>
2018-04-09 21:23:59 +0000

    generate channel

--- app/channels/web_notifications_channel.rb
+++ app/channels/web_notifications_channel.rb
@@ -1,9 +1,8 @@
 class WebNotificationsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "web_notifications_channel"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
 end
