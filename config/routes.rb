d76a9d0771330ac55a35269b84ec7df12b9a3c22
Viktharien <viktharien@zoho.com>
2018-04-09 21:41:38 +0000

    test actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'messages/index'  => 'messages#new'
   get 'messages/create'
   get 'page/index'
-  root 'page#index'
+  root 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
