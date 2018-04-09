102737b4fad025967cca496dd48f45b420a84547
Viktharien <viktharien@zoho.com>
2018-04-09 21:56:17 +0000

    test actioncable

--- app/channels/web_notifications_channel.rb
+++ app/channels/web_notifications_channel.rb
@@ -1,6 +1,8 @@
 class WebNotificationsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "web_notifications_channel"
+    message = Message.find(params[:id])
+    stream_for message
   end
 
   def unsubscribed
